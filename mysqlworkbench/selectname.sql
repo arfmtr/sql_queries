5f3b5429debd7589fde8ba06a2de8336170b5eb2
Arifa M <59384158+arfmtr@users.noreply.github.com>
2022-05-17 20:39:35 +0700

    Create selectname.sql

---
+++ mysqlworkbench/selectname.sql
@@ -0,0 +1,4 @@
+--Select last name of user, and total amount of purchases made by this user
+SELECT u.last_name, COUNT(*) AS purchases_amount FROM user u 
+JOIN purchase p ON u.id = p.fk_purchase_user
+GROUP BY u.last_name; 
