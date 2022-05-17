0e1ccf9c48da5841e820798996abe915df5238ef
Arifa M <59384158+arfmtr@users.noreply.github.com>
2022-05-17 20:45:58 +0700

    Update selectuser.sql

--- mysql/selectuser.sql
+++ mysql/selectuser.sql
@@ -1,5 +1,5 @@
 --Select last name of user, and total amount of purchases made by this user, only for users who made two or more purchases
-SELECT u.last_name, COUNT(*) AS total FROM user u 
-JOIN purchase p ON u.id = p.fk_purchase_user
-GROUP BY u.last_name
-HAVING total >= 2;
+SELECT t1.last_name, COUNT(*) AS total FROM user AS t1 
+JOIN purchase AS t2 ON t1.id = t2.fk_purchase_user 
+GROUP BY t1.last_name 
+HAVING total >= 2
