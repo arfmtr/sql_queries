f47615d98745ba9c435b296d9cf5839256458e0b
Arifa M <59384158+arfmtr@users.noreply.github.com>
2022-05-17 20:46:36 +0700

    Create selectuser.sql

---
+++ mysqlworkbench/selectuser.sql
@@ -0,0 +1,5 @@
+--Select last name of user, and total amount of purchases made by this user, only for users who made two or more purchases
+SELECT u.last_name, COUNT(*) AS purchases_amount FROM user u 
+JOIN purchase p ON u.id = p.fk_purchase_user
+GROUP BY u.last_name
+HAVING purchases_amount >= 2;
