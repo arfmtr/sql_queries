f393e3b43f589aa47c133dd87f3a450807583d42
Arifa M <59384158+arfmtr@users.noreply.github.com>
2022-05-18 11:16:54 +0700

    Update selectuser.sql

--- mysqlworkbench/selectuser.sql
+++ mysqlworkbench/selectuser.sql
@@ -1,5 +1,5 @@
 --Select last name of user, and total amount of purchases made by this user, only for users who made two or more purchases
-SELECT u.last_name, COUNT(*) AS purchases_amount FROM hwqueries.user u 
+SELECT u.last_name, COUNT(*) AS total FROM hwqueries.user u 
 JOIN hwqueries.purchase p ON u.id = p.fk_purchase_user
 GROUP BY u.last_name
-HAVING purchases_amount >= 2;
+HAVING total >= 2;
